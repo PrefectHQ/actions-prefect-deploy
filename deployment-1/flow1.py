bb372730ccacee65e3282b50906e2c21f215dc4e
Ravi Cheetirala <ravi.cheetirala@tlconsulting.com.au>
2024-01-31 10:14:21 +1100

    flow name changes

--- deployment-1/flow1.py
+++ deployment-1/flow1.py
@@ -3,7 +3,7 @@ import json
 import requests
 from prefect import flow
 
-@flow(name="Simple Flow", log_prints=True)
+@flow(name="Simple-Flow-1", log_prints=True)
 def call_api(url: str = "http://time.jsontest.com/"):
     """Sends a GET request to the provided URL and returns the JSON response"""
     resp = requests.get(url).json()
